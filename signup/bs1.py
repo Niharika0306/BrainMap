3d35a510f9dec2164564d83611ea510374d27882
Miriam John K <miriamjk.19@gmail.com>
2019-11-20 21:59:26 +0530

    videos

---
+++ signup/bs1.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from pytube import YouTube
+from flask import Flask, render_template,redirect,request,url_for,flash
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def videos():
+	base = "https://www.youtube.com/results?search_query="
+	qstring = "engineering+technology"
+
+	r = requests.get(base+qstring)
+	print(r)
+	page = r.text
+	soup=bs(page,'html.parser')
+
+	vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
+	print(vids)
+	videolist=[]
+	for v in vids:
+		tmp = 'https://www.youtube.com' + v['href']
+		tmp = tmp.replace("watch?v=", "embed/")
+		videolist.append(tmp)
+	return render_template("tempvid.html",video1 = videolist)
+	
+if __name__ == '__main__':
+    app.run()
